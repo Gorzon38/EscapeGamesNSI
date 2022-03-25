3307d935f9984c4597485dede7e0b9f22c9f4cdf
Gorzon38 <64686176+Gorzon38@users.noreply.github.com>
2022-03-25 15:10:26 +0100

    Update Player.gd

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -1,16 +1,28 @@
 extends KinematicBody
 
+export var speed = 14
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+export var fall_acceleration = 75
+
+var velocity = Vector3.ZERO
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
+	
+func _physics_process(delta):
+	# We create a local variable to store the input direction.
+	var direction = Vector3.ZERO
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+	# We check for each move input and update the direction accordingly.
+	if Input.is_action_pressed("move_right"):
+		direction.x += 1
+	if Input.is_action_pressed("move_left"):
+		direction.x -= 1
+	if Input.is_action_pressed("move_back"):
+		# Notice how we are working with the vector's x and z axes.
+		# In 3D, the XZ plane is the ground plane.
+		direction.z += 1
+	if Input.is_action_pressed("move_forward"):
+		direction.z -= 1
