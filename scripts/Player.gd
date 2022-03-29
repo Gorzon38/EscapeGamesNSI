6476be0257f85fe8e0855e15f9883abb11fe3cd0
Gorzon38 <64686176+Gorzon38@users.noreply.github.com>
2022-03-29 08:57:00 +0200

    Mouvement de cam

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -6,11 +6,20 @@ export var fall_acceleration = 75
 
 var velocity = Vector3.ZERO
 
+var mouse_sensitivity = 1.5
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-	
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+func _input(event):
+	# Look with the mouse
+	if event is InputEventMouseMotion:
+		rotation_degrees.y -= event.relative.x * mouse_sensitivity / 18
+		$Head.rotation_degrees.x -= event.relative.y * mouse_sensitivity / 18
+		$Head.rotation_degrees.x = clamp($Head.rotation_degrees.x, -90, 90)
+		
+
 func _physics_process(delta):
 	# We create a local variable to store the input direction.
 	var direction = Vector3.ZERO
@@ -20,9 +29,16 @@ func _physics_process(delta):
 		direction.x += 1
 	if Input.is_action_pressed("move_left"):
 		direction.x -= 1
-	if Input.is_action_pressed("move_back"):
-		# Notice how we are working with the vector's x and z axes.
-		# In 3D, the XZ plane is the ground plane.
+	if Input.is_action_pressed("move_backward"):
 		direction.z += 1
 	if Input.is_action_pressed("move_forward"):
 		direction.z -= 1
+		
+	if direction != Vector3.ZERO:
+		direction = direction.normalized()
+		$Pivot.look_at(translation + direction, Vector3.UP)
+
+	velocity.x = direction.x * speed
+	velocity.z = direction.z * speed
+	velocity.y -= fall_acceleration * delta
+	velocity = move_and_slide(velocity, Vector3.UP)
