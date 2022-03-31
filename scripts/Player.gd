a6a8e73cbdb100cf6bdee264601ea8ab1e04d33a
Gorzon38 <64686176+Gorzon38@users.noreply.github.com>
2022-03-31 14:25:34 +0200

    Fix movement

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -1,12 +1,12 @@
 extends KinematicBody
 
-export var speed = 14
+export var speed: float = 14
 
-export var fall_acceleration = 75
+export var fall_acceleration: float = 75
 
-var velocity = Vector3.ZERO
+var velocity: Vector3 = Vector3.ZERO
 
-var mouse_sensitivity = 1.5
+var mouse_sensitivity: float = 1.5
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -22,7 +22,9 @@ func _input(event):
 
 func _physics_process(delta):
 	# We create a local variable to store the input direction.
-	var direction = Vector3.ZERO
+	velocity.x = 0
+	velocity.z = 0
+	var direction = Vector2()
 
 	# We check for each move input and update the direction accordingly.
 	if Input.is_action_pressed("move_right"):
@@ -30,15 +32,20 @@ func _physics_process(delta):
 	if Input.is_action_pressed("move_left"):
 		direction.x -= 1
 	if Input.is_action_pressed("move_backward"):
-		direction.z += 1
+		direction.y += 1
 	if Input.is_action_pressed("move_forward"):
-		direction.z -= 1
+		direction.y -= 1
 		
-	if direction != Vector3.ZERO:
-		direction = direction.normalized()
-		$Pivot.look_at(translation + direction, Vector3.UP)
+	#f direction != Vector3.ZERO:
+	direction = direction.normalized()
+	#$Pivot.look_at(translation + direction, Vector3.UP)
+		
+	var forward = global_transform.basis.z
+	var right = global_transform.basis.x
+	
+	var relativeDir = (forward * direction.y + right * direction.x)
 
-	velocity.x = direction.x * speed
-	velocity.z = direction.z * speed
+	velocity.x = relativeDir.x * speed
+	velocity.z = relativeDir.z * speed
 	velocity.y -= fall_acceleration * delta
 	velocity = move_and_slide(velocity, Vector3.UP)
