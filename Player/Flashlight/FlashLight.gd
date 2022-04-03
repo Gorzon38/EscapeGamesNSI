5b25371b3714db9ab35953819a59d6090ed1a29b
Gorzon38 <64686176+Gorzon38@users.noreply.github.com>
2022-04-03 10:29:14 +0200

    Update FlashLight.gd

--- Player/Flashlight/FlashLight.gd
+++ Player/Flashlight/FlashLight.gd
@@ -4,10 +4,6 @@ const FLASHLIGHT_FOLLOW_SPEED: int = 15
 
 onready var light: SpotLight = $FlashlightMesh/SpotLight
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
 func turn_on_off() -> void:
 	light.visible = !light.visible
 	visible = !visible
