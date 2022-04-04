6be0b50012f56f664c55674f8728f01bd9763594
Gorzon38 <64686176+Gorzon38@users.noreply.github.com>
2022-04-04 09:24:33 +0200

    z

--- Player/Flashlight/FlashLight.gd
+++ Player/Flashlight/FlashLight.gd
@@ -2,7 +2,7 @@ extends Spatial
 
 const FLASHLIGHT_FOLLOW_SPEED: int = 15
 
-onready var light: SpotLight = $FlashlightMesh/SpotLight
+onready var light: SpotLight = $FlashlightMesh/Light
 
 func turn_on_off() -> void:
 	light.visible = !light.visible
